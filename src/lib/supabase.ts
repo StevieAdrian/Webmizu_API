075e895e2a25107b4a59a157bd6e5d2ca93efac0
StevieAdrian <adrianstevie0@gmail.com>
2026-04-03 23:19:49 +0700

    fix: disable session persistence and auto token refresh

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,13 @@ import config from '../config';
 
 const supabase: SupabaseClient = createClient(
   config.supabase.url,
-  config.supabase.serviceRoleKey
+  config.supabase.serviceRoleKey,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
 );
 
 export default supabase;
